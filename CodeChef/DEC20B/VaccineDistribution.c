0794e9730b2d5f5c3c9f2d17b90589ff6d7d3157
Akash2172 <65895796+Akash2172@users.noreply.github.com>
2020-12-15 20:50:39 +0530

    Create VaccineDistribution.c

---
+++ CodeChef/DEC20B/VaccineDistribution.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{
+    int num,n1,d1;
+    int min=0,maj=0;
+    int numDay;
+    int arr1[20000],arr2[200];
+    
+    scanf("%d",&num);
+    for (int i=0;i<num;i++)
+    {
+        min=0,maj=0;
+        scanf("%d%d",&n1,&d1);
+        for(int j=0;j<n1;j++)
+        {
+            scanf("%d",&arr1[j]);
+        }
+        for(int k=0;k<n1;k++)
+        {
+            if(arr1[k]<10 || arr1[k]>79)
+            {
+                min++;
+            }
+            else if(arr1[k]>9 || arr1[k]<80)
+            {
+                maj++;
+            }
+                      
+        }
+               
+        numDay=ceil(min/(float)d1) + ceil(maj/(float)d1) ;
+        arr2[i]=numDay;
+
+    }
+    for (int i=0;i<num;i++)
+    {
+        printf("%d\n",arr2[i]);
+    }
+    return 0;
+}
+	
+
+
+
