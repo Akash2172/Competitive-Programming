c5ba99586c948cd6e7930381e05135aa4166f033
Akash2172 <65895796+Akash2172@users.noreply.github.com>
2020-12-27 20:06:32 +0530

    Create Datatypes.cpp

---
+++ HackerRank/C++/Datatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    printf("%f\n",d);
+    printf("%lf",e);
+
+    
+    
+    return 0;
+}
