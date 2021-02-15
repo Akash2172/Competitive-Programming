db2e491f1671325d86130a12bb6c6ea5c5092560
Akash2172 <65895796+Akash2172@users.noreply.github.com>
2021-02-15 15:59:47 +0530

    Create HDIVSR.cpp

---
+++ CodeChef/HDIVSR.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define for(i,a,n) for(i=a;i<n;i++)
+#define ll unsigned long long
+using namespace std;
+
+int main()
+{
+    int a,out=1,i;
+    cin>>a;
+    for(i,1,11)
+    {
+        if(a%i==0)out=i;
+    }
+    cout<<out<<'\n';
+    return 0;
+}
