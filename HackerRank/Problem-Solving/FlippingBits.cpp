8b348863f4c04c7ba397788ded811ac2ee776bc8
Akash2172 <65895796+Akash2172@users.noreply.github.com>
2021-04-07 14:22:04 +0530

    Create FlippingBits.cpp

---
+++ HackerRank/Problem-Solving/FlippingBits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fob(i,a,n) for(int i=a;i<n;i++)
+#define ll unsigned int 
+#define MOD 1000000007
+using namespace std;
+
+
+void solve()
+{
+    ll a; //ll is unsigned int so thats important
+    cin>>a;
+    cout<<~a;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL),cout.tie(NULL);
+    int t;cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<'\n';
+    }
+    return 0;
+}
