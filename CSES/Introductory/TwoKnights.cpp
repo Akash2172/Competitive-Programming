34e52bfb5c2e9c017d32e9d28a81630e61082dfe
Akash Sharma <65895796+Akash2172@users.noreply.github.com>
2022-10-19 10:20:05 +0530

    Create TwoKnights.cpp

---
+++ CSES/Introductory/TwoKnights.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long n;
+    cin>>n;
+    for (long long i = 1; i <= n; ++i)
+    {
+        long long res = ((i-1)*(i+4)*(i*i-3*i+4))/2;
+        cout << res << '\n';
+    }
+    return 0;
+}
